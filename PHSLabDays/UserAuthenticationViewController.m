73a5c741260f7b0313c1cc301042105c432475f0
Ryan D'souza <dsouzarc@gmail.com>
2015-03-02 18:30:14 -0500

    Added support for notifications every morning at 6:25AM

--- PHSLabDays/UserAuthenticationViewController.m
+++ PHSLabDays/UserAuthenticationViewController.m
@@ -27,6 +27,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.passwordField.delegate = self;
+    
+    [self setNotificationForTomorrow];
+}
+
+- (void) setNotificationForTomorrow
+{
+    if ([UIApplication instancesRespondToSelector:@selector(registerUserNotificationSettings:)]) {
+        [[UIApplication sharedApplication] registerUserNotificationSettings:
+                        [UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeAlert | UIUserNotificationTypeSound
+                                                        categories:nil]];
+    }
+    
+    NSCalendar *calendar = [NSCalendar autoupdatingCurrentCalendar] ;
+    NSDate *now = [NSDate date];
+    
+    NSDateComponents *components = [calendar components:(NSCalendarUnitYear | NSCalendarUnitMonth |  NSCalendarUnitDay | NSCalendarUnitHour | NSCalendarUnitMinute) fromDate:now];
+    [components setHour:06];
+    [components setMinute:25];
+    
+    UILocalNotification *notification = [[UILocalNotification alloc]init];
+    notification.fireDate = [calendar dateFromComponents:components];
+    notification.repeatInterval = NSDayCalendarUnit;
+    [notification setAlertBody:@"Wake up!"];
+    [[UIApplication sharedApplication] scheduleLocalNotification:notification];
 }
 
 - (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
