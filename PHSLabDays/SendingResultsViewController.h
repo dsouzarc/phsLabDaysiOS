dcc8e1cc23f0ea95bcdd37569142b181e1a36a22
Ryan D'souza <dsouzarc@gmail.com>
2015-03-04 22:16:12 -0500

    UI For displaying results

---
+++ PHSLabDays/SendingResultsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SendingResultsViewController.h
+//  PHSLabDays
+//
+//  Created by Ryan D'souza on 3/4/15.
+//  Copyright (c) 2015 Ryan D'souza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SendingResultsViewController : UITabBarController
+
+@end
---
+++ PHSLabDays/SendingResultsViewController.m
@@ -0,0 +1,40 @@
+//
+//  SendingResultsViewController.m
+//  PHSLabDays
+//
+//  Created by Ryan D'souza on 3/4/15.
+//  Copyright (c) 2015 Ryan D'souza. All rights reserved.
+//
+
+#import "SendingResultsViewController.h"
+
+@interface SendingResultsViewController ()
+
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) IBOutlet UIButton *closeButton;
+
+@end
+
+@implementation SendingResultsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
